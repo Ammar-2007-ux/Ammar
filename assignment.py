b2d3b8a3eac3aebdf9629226cca9c30278cec3e0
Ammar-2007-ux <ammarhishama@gmail.com>
2024-11-25 15:32:55 +0000

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -9,20 +9,4 @@ import matplotlib.pyplot as plt
 DATA_FILE = "finance_data.csv"
 
 
-# Initialize the main window
-window = tk.Tk()
-window.geometry("600x800")
-window.title("Finance Tracker")
-
-
-# Utility functions
-def initialize_file():
-    """Initialize the CSV file if it doesn't exist."""
-    try:
-        with open(DATA_FILE, "x") as file:
-            writer = csv.writer(file)
-            writer.writerow(["Date", "Income", "Expense", "Category"])
-    except FileExistsError:
-        pass
-
 
